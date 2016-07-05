e22b58c30149bcc157c6dbbc6964d361c5634c1d
LARCHER Maxime <KN5391@d21.tes.local>
2016-07-05 11:39:46 +0200

    Changed the provisioning file.

---
+++ NUnitTestProject2/NUnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NUnitTestProject2
+{
+    //[TestFixture]
+    public class NUnitTest1
+    {
+        //[Test]
+        public void TestMethod1()
+        {
+            Assert.
+        }
+    }
+}
